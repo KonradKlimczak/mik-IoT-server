48b7e24397b8ea4a7b7aec62f63a448fe7b7fe79
Konrad <konradklimczak93@gmail.com>
2018-01-03 21:37:29 +0100

    eslint added.

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ const express = require('express');
 const app = express();
 
 const database = {
-    matex1: 0,
-    matex2: 0
+  matex1: 0,
+  matex2: 0
 };
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-
 app.get('/sock/:machine', (req, res) => {
-    const machineName = req.params.machine;
-    if (database[machineName] === undefined) {
-        return res.status(404).send(`Machine ${machineName} not found`);
-    }
-    return res.send(`${database[machineName]}`);
-})
+  const machineName = req.params.machine;
+  if (database[machineName] === undefined) {
+    return res.status(404).send(`Machine ${machineName} not found`);
+  }
+  return res.send(`${database[machineName]}`);
+});
 app.post('/sock/:machine', (req, res) => {
-    const machineName = req.params.machine;
-    if (database[machineName] === undefined) {
-        return res.status(404).send(`Machine ${machineName} not found`);
-    }
-    database[machineName] = database[machineName] + 1;
-    return res.send('ok');
-})
+  const machineName = req.params.machine;
+  if (database[machineName] === undefined) {
+    return res.status(404).send(`Machine ${machineName} not found`);
+  }
+  database[machineName] = database[machineName] + 1;
+  return res.send('ok');
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.info('Example app listening on port 3000!'));
