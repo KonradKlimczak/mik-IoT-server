e3eda0dec4875cdb3e059524601d14c58ed468a3
Konrad <konradklimczak93@gmail.com>
2018-01-09 20:34:04 +0100

    info and test endpoint added.

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 
 const database = {
+  connected: [],
   matex1: 0,
   matex2: 0
 };
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/info', (req, res) => {
+  const info = `Machines connected: ${database.connected.join(', ')}. Machine matex1 produced: ${database.matex1}, matex2 produced: ${database.matex2}`;
+  return res.send(info);
+});
 
 app.get('/sock/:machine', (req, res) => {
   const machineName = req.params.machine;
@@ -21,6 +27,15 @@ app.post('/sock/:machine', (req, res) => {
     return res.status(404).send(`Machine ${machineName} not found`);
   }
   database[machineName] = database[machineName] + 1;
+  return res.send('ok');
+});
+
+app.post('/test/:machine', (req, res) => {
+  const machineName = req.params.machine;
+  if (database[machineName] === undefined) {
+    return res.status(404).send(`Machine ${machineName} not found`);
+  }
+  database.connected.push(machineName);
   return res.send('ok');
 });
 
